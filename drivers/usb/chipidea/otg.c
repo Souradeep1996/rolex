f40a50611a8a85140b288f0f1fef124c01a70cb3
Souradeep1996 <srdp.bhattacharja@gmail.com>
2018-04-13 23:10:57 +0530

    Reverted Fastcharge, added Qnovo charge support.

--- drivers/usb/chipidea/otg.c
+++ drivers/usb/chipidea/otg.c
@@ -145,10 +145,3 @@ void ci_hdrc_otg_destroy(struct ci_hdrc *ci)
 		ci_hdrc_otg_fsm_remove(ci);
 }
 
-#ifdef CONFIG_FORCE_FAST_CHARGE
-	if (force_fast_charge == 1) {
-			mA = USB_FASTCHG_LOAD;
-			pr_info("USB fast charging is ON - 1000mA.\n");
-	} else {
-		pr_info("USB fast charging is OFF.\n");
-	}
